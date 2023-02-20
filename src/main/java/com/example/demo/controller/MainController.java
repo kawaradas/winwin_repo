ce4555ba4f7aad015cb72cb97b62b1eceaaddd0d
kawaradas <kawaradas@systena.co.jp>
2023-02-20 15:15:20 +0900

    fix

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -2,7 +2,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;  
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +16,7 @@ import com.example.demo.mapper.AnswerMapper;
 import com.example.demo.mapper.PostMapper;
 import com.example.demo.mapper.UserMapper;
 
-@Controller
+@RestController
 public class MainController {
 	
 	private int logined_userid;
